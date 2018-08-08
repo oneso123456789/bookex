da7e75b5720850bcbe0bd2b270b456c6beb767fe
crow <oneso123456789@gmail.com>
2018-08-08 20:15:57 +0900

    2018-08-08 8:15

--- ex01/src/main/java/org/zerock/service/BoardService.java
+++ ex01/src/main/java/org/zerock/service/BoardService.java
@@ -5,14 +5,14 @@ import java.util.List;
 import org.zerock.domain.BoardVO;
 
 public interface BoardService {
-	
+
 	public void regist(BoardVO board) throws Exception;
-	
+
 	public BoardVO read(Integer bno) throws Exception;
-	
+
 	public void modify(BoardVO board) throws Exception;
-	
+
 	public void remove(Integer bno) throws Exception;
-	
-	public List<BoardVO> listall() throws Exception;
+
+	public List<BoardVO> listAll() throws Exception;
 }
