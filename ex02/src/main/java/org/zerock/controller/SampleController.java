c36946105b9a32634a282122c3d9118e410d4656
oneso123456789 <40660368+oneso123456789@users.noreply.github.com>
2018-09-19 03:35:36 +0900

    메모리 누수failed to load class com.mysql.jdbc.nonregisteringdriver 해결하니까 다른 메모리 누수 생김..

---
+++ ex02/src/main/java/org/zerock/controller/SampleController.java
@@ -0,0 +1,15 @@
+package org.zerock.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+	
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+}
