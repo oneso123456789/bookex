ab65afa6ba18266ecf9ed3eddce10fbfcaf3c39b
oneso123456789 <40660368+oneso123456789@users.noreply.github.com>
2018-09-24 02:54:13 +0900

    mysql 문제 해결함 그리고 데이터베이스 사용할꺼면  resources 폴더에 lo4파일들 생성하기 수정한부분은:root-context.xml에 dataSsource에서 url property value값 뒤에 "useSSL=false&amp;serverTimezone=UTC" 붙임 그리고 pom.xml에서 mysql-connector-java 버전을 6.0.5로 만듬 안그러면 드라이버를 못찾아서 데이터베이스에 값을 못넣어줌

---
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -0,0 +1,41 @@
+package org.zerock.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.ReplyVO;
+import org.zerock.service.ReplyService;
+
+@RestController
+@RequestMapping("/replies")
+public class ReplyController {
+
+	@Inject
+	private ReplyService service;
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<String> register(@RequestBody ReplyVO vo){
+		 
+		ResponseEntity<String> entity = null;
+		
+		try {
+			service.addReply(vo);
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}  
+   
+} 
