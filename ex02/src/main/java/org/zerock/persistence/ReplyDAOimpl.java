bea3bbd9535d76f8bd0dbe2bfaf490bfea77b94c
oneso123456789 <40660368+oneso123456789@users.noreply.github.com>
2018-09-28 20:48:12 +0900

    2018 09 28 Replycotroller listPage부분추가

--- ex02/src/main/java/org/zerock/persistence/ReplyDAOimpl.java
+++ ex02/src/main/java/org/zerock/persistence/ReplyDAOimpl.java
@@ -1,11 +1,14 @@
 package org.zerock.persistence;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 
 @Repository
@@ -43,6 +46,23 @@ public class ReplyDAOimpl implements ReplyDAO{
 		// TODO Auto-generated method stub
 		
 		session.delete(namespace + ".delete", rno);
+	}
+
+	@Override
+	public List<ReplyVO> listPage(Integer bno, Criteria cri) throws Exception {
+		
+		Map<String, Object> paramMap = new HashMap<>();
+		
+		paramMap.put("bno", bno);
+		paramMap.put("cri", cri);
+		
+		return session.selectList(namespace + ".listPage", paramMap);
+	}
+
+	@Override
+	public int count(Integer bno) throws Exception {
+		
+		return session.selectOne(namespace + ".count", bno);
 	}
 	
 	
--- ex02/src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ ex02/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.persistence.ReplyDAO;
 
@@ -36,6 +37,18 @@ public class ReplyServiceImpl implements ReplyService{
 	public void removeReply(Integer rno) throws Exception {
 		// TODO Auto-generated method stub
 		 dao.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> listReplyPage(Integer bno, Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.listPage(bno, cri);
+	}
+
+	@Override
+	public int count(Integer bno) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.count(bno);
 	}
 	
 	
