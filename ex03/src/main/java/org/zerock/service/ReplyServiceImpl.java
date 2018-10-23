7126365762d6a24b07e95976a9d1a247d237fdea
oneso123456789 <40660368+oneso123456789@users.noreply.github.com>
2018-10-24 04:19:04 +0900

    mapper.xml의 namespace와 DAOImpl의 namesapce를 일치시켜야함

---
+++ ex03/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -0,0 +1,56 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
+import org.zerock.persistence.ReplyDAO;
+
+@Service
+public class ReplyServiceImpl implements ReplyService{
+	
+	@Inject
+	private ReplyDAO dao;
+
+	@Override
+	public void addReply(ReplyVO vo) throws Exception {
+		// TODO Auto-generated method stub
+		dao.create(vo);
+	}
+
+	@Override
+	public List<ReplyVO> listReply(Integer bno) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.list(bno);
+	}
+
+	@Override
+	public void modifyReply(ReplyVO vo) throws Exception {
+		// TODO Auto-generated method stub
+		dao.update(vo);
+	}
+
+	@Override
+	public void removeReply(Integer rno) throws Exception {
+		// TODO Auto-generated method stub
+		 dao.delete(rno);
+	}
+
+	@Override
+	public List<ReplyVO> listReplyPage(Integer bno, Criteria cri) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.listPage(bno, cri);
+	}
+
+	@Override
+	public int count(Integer bno) throws Exception {
+		// TODO Auto-generated method stub
+		return dao.count(bno);
+	}
+	
+	
+
+}
